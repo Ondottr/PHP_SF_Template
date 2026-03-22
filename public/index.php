79894173a2dc1b9d350b16dba1cddc622d68b066
Dmytro Dyvulskyi <dmytro.dyvulskyi@medserv.ie>
2026-03-22 10:36:05 +0100

    cosmetics

--- public/index.php
+++ public/index.php
@@ -12,7 +12,6 @@
  * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-use App\Entity\User;
 use App\Kernel;
 use PHP_SF\Framework\Http\Middleware\auth;
 use PHP_SF\System as PHP_SF;
@@ -43,9 +42,9 @@ if ( DEV_MODE )
 
 require_once __DIR__ . '/../config/eventListeners.php';
 
-( new Dotenv )->bootEnv( __DIR__ . '/../.env' );
+( new Dotenv() )->bootEnv( __DIR__ . '/../.env' );
 
-$kernel = ( new PHP_SF\Kernel )
+$kernel = ( new PHP_SF\Kernel() )
     ->addTranslationFiles( __DIR__ . '/../lang' )
     ->addControllers( __DIR__ . '/../App/Http/Controller' )
     ->setHeaderTemplateClassName( header::class )
