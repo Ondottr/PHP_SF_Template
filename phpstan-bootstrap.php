7bda65f17d8c70de4061a863c76f67a229b48c6f
Dmytro Dyvulskyi <dmytro.dyvulskyi@medserv.ie>
2026-06-24 16:26:54 +0200

    wip

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Constants required by App code at parse time
+defined('DEV_MODE') || define('DEV_MODE', true);
+defined('DEFAULT_LOCALE') || define('DEFAULT_LOCALE', 'en');
