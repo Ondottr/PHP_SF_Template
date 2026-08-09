71d27da939a612783f149c28e52a440a1f3bd12f
Dmytro Dyvulskyi <dmytro.dyvulskyi@medserv.ie>
2026-08-09 11:49:02 +0200

    feat!: v4 — migrate to Symfony 8.1 / PHP 8.5; require php-sf ^4.0 and php-sf-cache ^3.0
    - deps: symfony/* ^8.1, php 8.5.*, doctrine-bundle ^3.0, php-sf-cache ^3.0, php-simple-framework ^4.0; drop proxy-manager-bridge + package-versions-deprecated
    - doctrine: replace removed proxy opts with enable_native_lazy_objects
    - routing: migrate framework/web-profiler error resources .xml -> .php
    - command: TaggedIterator -> AutowireIterator; DoctrineCommand::getEntityManager() -> getDoctrine()->getManager()
    - controllers: Routing\Annotation\Route -> Routing\Attribute\Route (incl. staging overlay CRUD)
    - bin/console: wire setApplicationUserClassName(User::class)
    - init.sh: drop deprecated TranslatablePropertyName attribute
    - phpstan: regenerate baseline; mark migrations (?) optional
    - test: add CrudHomeCest covering the /crud home page and all section links
    
    Signed-off-by: Dmytro Dyvulskyi <dmytro.dyvulskyi@medserv.ie>

--- staging/overlay/App/Http/SymfonyControllers/PostCrudSymfonyController.php
+++ staging/overlay/App/Http/SymfonyControllers/PostCrudSymfonyController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Csrf\CsrfToken;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
